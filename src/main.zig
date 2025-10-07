be5746ab5f867370569f6d253d51cd02c5b7619c
NeumontAGrover <agrover@student.neumont.edu>
2025-10-06 20:55:06 -0600

    Opened GLFW Window

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,18 @@
 const std = @import("std");
+const glfw = @import("headers/glfw.zig");
 
 pub fn main() !void {
-    std.debug.print("Hello World!\n", .{});
+    _ = glfw.glfwInit();
+    defer glfw.glfwTerminate();
+
+    glfw.glfwWindowHint(glfw.GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfw.glfwWindowHint(glfw.GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfw.glfwWindowHint(glfw.GLFW_OPENGL_PROFILE, glfw.GLFW_OPENGL_CORE_PROFILE);
+
+    const window = glfw.glfwCreateWindow(800, 600, "Hello World Window", null, null);
+    defer glfw.glfwDestroyWindow(window);
+
+    while (glfw.glfwWindowShouldClose(window) == 0) {
+        glfw.glfwPollEvents();
+    }
 }
